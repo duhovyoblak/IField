de2da0874f07839e1375cc676fea7481568686c7
Pavol Horansky <duhovyoblak@gmail.com>
2022-04-23 11:39:08 +0200

    A je to server !

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import sys
 sys.path.append('lib')
 
 from siqo_lib      import journal
+from server        import srv
 
 #==============================================================================
 # package's constants
