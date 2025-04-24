2c8299d407227c94200607527c5215980ec4088d
Pavol Horansky <duhovyoblak@gmail.com>
2025-04-24 18:23:16 +0200

    ComplexPoint base completed

--- src/siqo_cpoint.py
+++ src/siqo_cpoint.py
@@ -1,9 +1,9 @@
 #==============================================================================
 # Siqo class ComplexPoint and CompplexField
 #------------------------------------------------------------------------------
-#import math
+import math
 import cmath
-import random                as rnd
+import random                 as rnd
 #import siqo_general          as gen
 
 #==============================================================================
@@ -26,8 +26,10 @@ class ComplexPoint:
     def __init__(self, *, pos=[], c=complex(0,0)):
         "Calls constructor of ComplexPoint on respective position"
         
-        self.pos = list(pos)      # Vector of real numbers for position coordinates
         self.c   = c              # Complex value of this ComplexPoint
+        self.pos = list(pos)      # Vector of real numbers for position coordinates
+        self.omg = 0              # Omega uhlova rychlost [rad/s] 
+        self.phs = 0              # Total faza [rad]              
 
     #--------------------------------------------------------------------------
     def __str__(self):
@@ -46,8 +48,10 @@ class ComplexPoint:
 
         dat['c'  ] = self.c
         dat['pos'] = self.pos
+        dat['omg'] = self.omg
+        dat['phs'] = self.phs
 
-        msg.append(f"{indent*_IND}{self.posStr()} : {self.cStr()}")
+        msg.append(f"{indent*_IND}{self.posStr()}: {self.cStr()} ({self.phs:5.3f} rad @ {self.omg:5.3f} rad/s)")
 
         return {'res':'OK', 'dat':dat, 'msg':msg}
         
@@ -60,8 +64,8 @@ class ComplexPoint:
         i = 0
         for coor in self.pos:
             
-            if i == 0: toRet +=   f"{coor:7.2f}"
-            else     : toRet += f" |{coor:7.2f}"
+            if i == 0: toRet +=   f"{coor:5.2f}"
+            else     : toRet += f" |{coor:5.2f}"
             i += 1
 
         toRet += ']'
@@ -72,13 +76,16 @@ class ComplexPoint:
     def cStr(self):
         "Creates string representation of the complex value of this ComplexPoint"
 
-        return f"({self.c.real:7.3f} {self.c.imag:7.3f}j)"
+        return f"({self.c.real:6.3f} {self.c.imag:6.3f}j)"
     
     #--------------------------------------------------------------------------
     def copy(self):
         "Creates copy of this ComplexPoint"
 
         toRet = ComplexPoint(pos=self.pos, c=self.c)
+        toRet.omg = self.omg
+        toRet.phs = self.phs
+
         return toRet
         
     #==========================================================================
@@ -87,31 +94,35 @@ class ComplexPoint:
     def clear(self):
         "Sets complex number to (0+0j)"
         
-        self.c = complex(0,0)
+        self.c   = complex(0,0)
+        self.omg = 0            # Omega uhlova rychlost [rad/s]
+        self.phs = 0            # Total faza [rad]
+
         return self
+
     #--------------------------------------------------------------------------
-    def setComp(self, c):
+    def setComp(self, c:complex):
         "Sets complex number to respective complex value"
         
         self.c = c
         return self
         
     #--------------------------------------------------------------------------
-    def setRect(self, re, im):
+    def setRect(self, re:float, im:float):
         "Sets complex number given by rectangular coordinates (real, imag)"
         
         self.c = complex(re, im)
         return self
         
     #--------------------------------------------------------------------------
-    def setPolar(self, mod, phi):
+    def setPolar(self, mod:float, phi:float):
         "Sets complex number given by polar coordinates (modulus, phase)"
         
         self.c = cmath.rect(mod, phi)
         return self
         
     #--------------------------------------------------------------------------
-    def rndBit(self, prob):
+    def rndBit(self, prob:float):
         "Sets real value 0/1 with respective probability and imaginary value sets to 0"
 
         x = rnd.randint(0, 9999)
@@ -204,6 +215,14 @@ class ComplexPoint:
         for r in dlts: toRet += r*r
         
         return toRet
+
+    #--------------------------------------------------------------------------
+    def distTo(self, toP):
+        "Returns the distance to respective ComplexPoint"
+        
+        sqrDist = self.distSqrTo(toP)
+        return math.sqrt(sqrDist)
+    
 
 #------------------------------------------------------------------------------
 print('ComplexPoint ver 3.01')
