325d375822a01c6fdc776a584a996e09ebefb81c
Pavol Horansky <duhovyoblak@gmail.com>
2026-06-23 23:06:34 +0200

    autoCorr method

--- src/ifield/ifield_line.py
+++ src/ifield/ifield_line.py
@@ -47,7 +47,7 @@ class InfoFieldLine(InfoMatrix):
         # Inicializacia
         #----------------------------------------------------------------------
         self.setIpType('ipIntLine')
-        self.setSchema({'axes': {'l': 'Lambda'}, 'vals': {'s': 'State', 'd': 'Delta', 'a': 'Autocorr'}})
+        self.setSchema({'axes': {'l': 'Lambda'}, 'vals': {'s': 'State', 'd': 'Delta', 'c': 'Autocorr', 'a': 'Amplitude'}})
         self.init(cnts={'l':_LAMBDA})
 
         self.applyMatrixMethod(methodKey='Integer random uniform', valueKey='s', params={'min':0, 'max':_AMP})
@@ -64,6 +64,7 @@ class InfoFieldLine(InfoMatrix):
         methods = super().mapSetMethods()
 
         methods['ILine deltas'      ] = {'matrixMethod': self.deltas,    'pointMethod':None, 'params':{}                               , 'type':'ask'  }
+        methods['ILine autocorr'    ] = {'matrixMethod': self.autoCorr,  'pointMethod':None, 'params':{}                               , 'type':'ask'  }
         methods['ILine epoch step'  ] = {'matrixMethod': self.epochStep, 'pointMethod':None, 'params':{}                               , 'type':'ask'  }
 
         for methodKey in methods.keys():
@@ -77,46 +78,90 @@ class InfoFieldLine(InfoMatrix):
     def deltas(self, valueKey:str, params:dict):
         """Compute deltas of states between consecutive points."""
 
-        logger.info(f"{self.name}.deltas: for key '{valueKey}' with params {params}")
+        logger.info(f"{self.name}.deltas: for key 'd' with params {params}")
         pts = 0
 
         #----------------------------------------------------------------------
         # Vsetky IPoints nastavim do subMatrix listu
         #----------------------------------------------------------------------
         points = self.actSubmatrix()
-        prevL = -1
+
         prevS = 0
+        points[0].set( vals = {'d': prevS} )
 
         #----------------------------------------------------------------------
         # Prejdem vsetky boby v subMatrix a pre kazdy bod nastavim hodnotu ako rozdiel medzi hodnotou bodu a predosleho bodu
         #----------------------------------------------------------------------
-        for point in points:
+        for i in range(1, len(points)):
 
-            currL = point.pos(axeKey='l')
+            point = points[i]
             currS = point.val(valKey='s')
-
-            #------------------------------------------------------------------
-            # Kontrola, ci sa body nachadzaju v poradi podla osi Lambda
-            #------------------------------------------------------------------
-            if currL != prevL + 1:
-                logger.error(f"{self.name}.deltas: Lambda {currL} is not in order after {prevL}, method skipped")
-                break
 
             #------------------------------------------------------------------
             # Vypocet a nastavenie delty
             #------------------------------------------------------------------
             delta = currS - prevS
-            point.set( vals = {valueKey: delta})
+            point.set( vals = {'d': delta})
 
             #------------------------------------------------------------------
             # Posun na nasledujuci bod
             #------------------------------------------------------------------
-            prevL = currL
             prevS = currS
             pts += 1
 
         #----------------------------------------------------------------------
         logger.info(f"{self.name}.deltas: {pts} InfoPoints was updated for key '{valueKey}' in deltas")
+
+    #--------------------------------------------------------------------------
+    def autoCorr(self, valueKey:str, params:dict):
+        """Compute auto-correlation of states."""
+
+        logger.info(f"{self.name}.autoCorr: for key 'c' with params {params}")
+
+        #----------------------------------------------------------------------
+        # Vsetky IPoints nastavim do subMatrix listu
+        #----------------------------------------------------------------------
+        points = self.actSubmatrix()
+        n = len(points)
+
+        #----------------------------------------------------------------------
+        # Prejdem tau od 0 po N-1, kde N je pocet bodov v subMatrixe
+        #----------------------------------------------------------------------
+        for tau in range(n-1):
+
+            suma = 0
+            prod = 0
+            maxP = 0
+
+            #------------------------------------------------------------------
+            # Prejdem dvojice bodov (i, i+tau) a pre kazdu dvojicu bodov vypocitam produkt ich hodnot a pripoctem k sume
+            #------------------------------------------------------------------
+            for i in range(n):
+
+                # Pouzijem modularnu algebru, aby som pre lub
+                iPos =  i        % n
+                jPos = (i + tau) % n
+
+                iVal = points[iPos].val(valKey='d')
+                jVal = points[jPos].val(valKey='d')
+
+                prod = iVal * jVal
+                if prod > maxP: maxP = prod
+
+                suma += prod
+
+            #------------------------------------------------------------------
+            # Normujem sumu podla poctu bodov a nastavim hodnotu auto-korelacie pre tau
+            #------------------------------------------------------------------
+            points[tau].set(vals={'c': (suma/n) })
+
+        #----------------------------------------------------------------------
+        # Posledny bod nastavim na 0
+        #----------------------------------------------------------------------
+        points[n-1].set(vals={'c': 0})
+
+        #----------------------------------------------------------------------
+        logger.info(f"{self.name}.autoCorr: Done")
 
     #--------------------------------------------------------------------------
     def epochStep(self, valueKey:str, params:dict):
